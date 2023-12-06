d469ec5eba86d3baa86ec4b95db8db7354aa87a7
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-12-06 16:02:41 +0700

    edit register

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,35 +1,62 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import Logo from '../utils/logo.svg'
+import Logo from '../assets/logo.svg';
 import { useEffect, useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios"
+import { registerRoute } from '../utils/APIRoutes';
 function Register() {
     const [values, setValues] = useState({
-        username: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
     });
 
-    const handleSubmit = (event) => {
+    const toastOptions = {
+        position: 'bottom-right',
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: 'dark',
+    };
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        alert("form");
+        // validasi login, jika ada error return false, jika tidak ada call api
+        if (handleValidation()) {
+            const { password, confirmPassword, username, email } = values;
+
+            const { data } = await axios.post(registerRoute, {
+                username,
+                email,
+                password,
+            })
+        };
     };
 
     const handleValidation = () => {
         const { password, confirmPassword, username, email } = values;
-        //Menit 44
+
         if (password !== confirmPassword) {
-            toast.error("password and confirm password should be same", {
-                position: "bottom-right",
-                autoClose: 8000,
-                pauseOnHover: true,
-                draggable: true,
-                theme: "dark",
-            })
+            toast.error('password and confirm password should be same', toastOptions);
+            return false;
+        } else if (username.length < 3) {
+            toast.error('Username should be greater than 3 characters', toastOptions);
+            return false;
+        } else if (password.length < 8) {
+            toast.error('Password should be equal or greater than 8 characters', toastOptions);
+            return false;
+        } else if (email === '') {
+            toast.error('Email is required', toastOptions);
+            return false;
         }
-    }
+
+        return true;
+    };
+
     const handleChange = (event) => {
         setValues({ ...values, [event.target.name]: event.target.value });
     };
@@ -37,22 +64,24 @@ function Register() {
     return (
         <>
             <FormContainer>
-                <form onSubmit={(event) => handleSubmit(event)} >
+                <form onSubmit={(event) => handleSubmit(event)}>
                     <div className="brand">
                         <img src={Logo} alt="Logo" />
                         <h1>Snappy</h1>
                     </div>
-                    <input type="text" placeholder="Username" name="username" onChange={e => handleChange(e)} />
-                    <input type="email" placeholder="Email" name="email" onChange={e => handleChange(e)} />
-                    <input type="password" placeholder="Password" name="password" onChange={e => handleChange(e)} />
-                    <input type="password" placeholder="Confirm Password" name="confirmPassword" onChange={e => handleChange(e)} />
-                    <button type='submit'>Create User</button>
-                    <span>Already have an account ? <Link to="/login">Login</Link></span>
+                    <input type="text" placeholder="Username" name="username" onChange={(e) => handleChange(e)} />
+                    <input type="email" placeholder="Email" name="email" onChange={(e) => handleChange(e)} />
+                    <input type="password" placeholder="Password" name="password" onChange={(e) => handleChange(e)} />
+                    <input type="password" placeholder="Confirm Password" name="confirmPassword" onChange={(e) => handleChange(e)} />
+                    <button type="submit">Create User</button>
+                    <span>
+                        Already have an account ? <Link to="/login">Login</Link>
+                    </span>
                 </form>
             </FormContainer>
             <ToastContainer />
         </>
-    )
+    );
 }
 
 const FormContainer = styled.div`
@@ -122,4 +151,5 @@ const FormContainer = styled.div`
             }
         }   
 `;
-export default Register
+
+export default Register;
